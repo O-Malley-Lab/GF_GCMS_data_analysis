e9802827f20829dc17a42e2e0681285cb90ff494
Lazarina Butkovich <lbutkovich@ucsb.edu>
2024-10-04 13:38:58 -0700

    Script 1 remove all steps for normalizing CC and S4 by cell pellet weights

--- Script_1_MSDIAL_stats.py
+++ Script_1_MSDIAL_stats.py
@@ -1,17 +1,15 @@
 """
 GF GCMS Data Analysis Script 1
-Lazarina Butkovich 7/19/24
+Lazarina Butkovich 10/4/24
 
-This script uses MS-DIAL outputs for GC-MS analysis to generate statistics to describe significant differences of metabolite amounts between samples. Two normalization methods will be used:
-(1) Normalize CC and AR peak area values based on cell pellet weight data. This script takes the peak area values from 'MSDIAL_area_output.xlsx' and the cell pellet data from 'GF_cell_pellet_weights.xlsx' to do this normalization.
-(2) Normalize all peak area values based on TIC sum (already done by MS-DIAL; these are the data in 'MSDIAL_norm_TIC_output.xlsx')
+This script uses MS-DIAL outputs for GC-MS analysis to generate statistics to describe significant differences of metabolite amounts between samples. The following normalization method is used: 
+ - Normalize all peak area values based on TIC sum (already done by MS-DIAL; these are the data in 'MSDIAL_norm_TIC_output.xlsx')
 
 As output, this script exports a clean excel file with relevant statistics:
-(1) Using cell pellet weight normalization, generate t test p-values to describe which metabolites are significantly present in CC and not AR (p_val_CC_vs_AR)
-(2) Using TIC sum normalization, generate t test p-values to describe which metabolites are significantly present in CC and not MC (p_val_CC_vs_MC), and which metabolites are significantly present in AR and not MC (p_val_AR_vs_MC).
+ - Using TIC sum normalization, generate t test p-values to describe which metabolites are significantly present in CC and not MC (p_val_CC_vs_MC), and which metabolites are significantly present in AR and not MC (p_val_AR_vs_MC).
 
 
-***Prior to using output data tables:
+***Prior to using MS-DIAL-output data tables:
 MS-DIAL: re-format so that the table does not have the top rows that are inconsistent with the rest of the format. Rename the average and standard deviation columns for samples to prevent them from having the same name (ie: rewrite as 'AR_avg' and'AR_std" instead of 'AR' and 'AR').
 
 """
@@ -275,10 +273,6 @@ FILENAME_MSDIAL_OUTPUT_NORM_TIC = 'MSDIAL_norm_TIC_output.xlsx'
 # MSDIAL output file with peak area data
 FILENAME_MSDIAL_OUTPUT_AREA = 'MSDIAL_area_output.xlsx'
 
-# Cell pellet weight data for direct comparison of CC and AR relative peak intensities
-FILENAME_CELL_PELLET_WEIGHTS = 'GF_cell_pellet_weights.xlsx'
-# Column names: 'Sample', 'Sample Mass mg'
-
 # Dictionary of tuples to describe pre- and post- strings in sample names (the middle part is 1 to n, where n=3 or 4 for biological (for AR and CC) or technical (for BLANK, MC, RF) replicates)
 SAMPLE_NAME_PRE_POST_STRS_DICT = {'CC':('OMALL_RFS_CC','_M'),'AR':('OMALL_RFS_AR_S4_','_M'),'MC':('OMALL_RFS_MC','_M'),'RF':('OMALL_RFS_RF','_M'), 'FAMES':('GCMS_FAMES_0','_GCMS01_20201209'), 'BLANK':('GCMS_BLANK_0','_GCMS01_20201209')}
 REPLICATE_NUMS = {'CC':4, 'AR':4, 'MC':4, 'RF':4,'FAMES':1,'BLANK':3}
@@ -288,17 +282,14 @@ OUTPUT_FILENAME = 'MSDIAL_stats.xlsx'
 COLS_NAME_CONVERTER = {'Alignment ID': 'Alignment_ID_MSDIAL','Average Rt(min)':'RT', 'Precursor_MZ':'EI_spectra_quant_mass', 'Quant mass': 'Quant_mass', 'Compound_Name':'Compound_Name_GNPS','MQScore':'MQScore_GNPS', 'Smiles':'SMILES_GNPS', 'INCHI':'INCHI_GNPS', 'Metabolite name': 'Metabolite_name_MSDIAL', 'SMILES':'SMILES_MSDIAL', 'INCHI':'INCHI_GNPS', 'molecular_formula':'molecular_formula_GNPS', 'npclassifier_superclass':'npclassifier_superclass_GNPS', 'npclassifier_class':'npclassifier_class_GNPS', 'npclassifier_pathway':'npclassifier_pathway_GNPS','Compound_Source':'Compound_Source_GNPS', 'Data_Collector':'Data_Collector_GNPS', 'Instrument':'Instrument_GNPS', 'Total spectrum similarity': 'Total_spectrum_similarity_MSDIAL'}
 
 COLS_TO_KEEP_SUMMARY_OUTPUT = ['shared name', 'Alignment_ID_MSDIAL', 'RT', 'Quant_mass', 'Metabolite_name_MSDIAL', 'Total_spectrum_similarity_MSDIAL',  'SMILES_MSDIAL', 
-'p_val_CC_vs_AR_cell_norm', 'log2_FC_CC_vs_AR_cell_norm',
 'p_val_CC_vs_AR', 'log2_FC_CC_vs_AR',
 'p_val_CC_vs_MC', 'log2_FC_CC_vs_MC',
 'p_val_AR_vs_MC', 'log2_FC_AR_vs_MC',
 'p_val_CC_vs_BLANK', 'log2_FC_CC_vs_BLANK',
 'p_val_AR_vs_BLANK', 'log2_FC_AR_vs_BLANK',
 'p_val_FAMES_vs_BLANK', 'log2_FC_FAMES_vs_BLANK',
-'CC_TIC_norm_avg', 'CC_TIC_norm_std', 
-'CC_cell_norm_avg', 'CC_cell_norm_std',
+'CC_TIC_norm_avg', 'CC_TIC_norm_std',
 'AR_TIC_norm_avg', 'AR_TIC_norm_std',
-'AR_cell_norm_avg', 'AR_cell_norm_std',
 'MC_TIC_norm_avg', 'MC_TIC_norm_std',
 'RF_TIC_norm_avg', 'RF_TIC_norm_std', 
 'FAMES_TIC_norm_avg', 'FAMES_TIC_norm_std',
@@ -316,7 +307,6 @@ Import data tables
 # Import data tables
 df_msdial_norm_tic = pd.read_excel(pjoin(INPUT_FOLDER, FILENAME_MSDIAL_OUTPUT_NORM_TIC))
 df_msdial_area = pd.read_excel(pjoin(INPUT_FOLDER, FILENAME_MSDIAL_OUTPUT_AREA))
-df_cell_pellet_weights = pd.read_excel(pjoin(INPUT_FOLDER, FILENAME_CELL_PELLET_WEIGHTS))
 
 # Ensure data tables are ordered by 'Alignment ID' column
 df_msdial_norm_tic.sort_values('Alignment ID', inplace=True)
@@ -351,53 +341,6 @@ for key in SAMPLE_NAME_PRE_POST_STRS_DICT:
             sample_groups_dict[key].append(col_name)
         else:
             sample_groups_dict[key] = [col_name]
-
-
-"""
-Normalize Peak Area Data Based on Cell Pellet Data
-"""
-# Keep only the CC and AR columns from the peak area data, as well as the key column
-# In sample_groups_dict, keep only column names that are values of the keys 'CC' and 'AR'
-cols_to_keep_cell_norm = []
-sample_cols_exp = []
-# Add the key column to the list of columns to keep
-cols_to_keep_cell_norm.append(KEY_COL)
-
-for key in sample_groups_dict:
-    if key == 'CC' or key == 'AR':
-        cols_to_keep_cell_norm += sample_groups_dict[key]
-        sample_cols_exp += sample_groups_dict[key]
-
-# Keep only the columns to keep
-df_msdial_area_cell_norm = df_msdial_area.copy()
-df_msdial_area_cell_norm = df_msdial_area_cell_norm[cols_to_keep_cell_norm]
-
-# Match the sample columns to the rows in the cell pellet data. Divide the data values in the sample columns by the 'Sample Mass mg' value in the cell pellet data.
-for col in sample_cols_exp:
-    mass = df_cell_pellet_weights.loc[df_cell_pellet_weights['Sample'] == col, 'Sample Mass mg'].values[0]
-    df_msdial_area_cell_norm[col] = df_msdial_area_cell_norm[col]/mass
-
-
-"""
-Generate Statistics for CC and AR Peak Area Data Normalized by Cell Pellet Weight
-"""
-# Create CC_cell_norm_avg and AR_cell_norm_avg columns
-df_msdial_area_cell_norm['CC_cell_norm_avg'] = df_msdial_area_cell_norm[sample_groups_dict['CC']].mean(axis=1)
-df_msdial_area_cell_norm['AR_cell_norm_avg'] = df_msdial_area_cell_norm[sample_groups_dict['AR']].mean(axis=1)
-
-# Create CC_cell_norm_std and AR_cell_norm_std columns
-df_msdial_area_cell_norm['CC_cell_norm_std'] = df_msdial_area_cell_norm[sample_groups_dict['CC']].std(axis=1)
-df_msdial_area_cell_norm['AR_cell_norm_std'] = df_msdial_area_cell_norm[sample_groups_dict['AR']].std(axis=1)
-
-# # Generate t test p-values for CC vs AR; note that the p-values are generated using the cell normalized data
-# p_val_CC_vs_AR = []
-# for index, row in df_msdial_area_cell_norm.iterrows():
-#     p_val = ttest_ind(row[sample_groups_dict['CC']], row[sample_groups_dict['AR']])[1]
-#     p_val_CC_vs_AR.append(p_val)
-# df_msdial_area_cell_norm['p_val_CC_vs_AR_cell_norm'] = p_val_CC_vs_AR
-
-generate_pval_col(df_msdial_area_cell_norm, sample_groups_dict, 'CC', 'AR', suffix = '_cell_norm')
-generate_log2_fc_col(df_msdial_area_cell_norm, 'CC', 'AR', data_col_prefix = '_cell_norm_avg', suffix = '_cell_norm')
 
 
 """
@@ -455,14 +398,6 @@ cols_to_add_tic = ['shared name',
 
 combine_dfs(df_msdial_summary, df_msdial_norm_tic_stats, cols_to_add_tic, KEY_COL, KEY_COL)
 
-# Add columns from df_msdial_area_cell_norm
-cols_to_add_cell_norm = ['shared name',
-    'p_val_CC_vs_AR_cell_norm', 'log2_FC_CC_vs_AR_cell_norm',
-    'CC_cell_norm_avg', 'CC_cell_norm_std',
-    'AR_cell_norm_avg', 'AR_cell_norm_std']
-
-combine_dfs(df_msdial_summary, df_msdial_area_cell_norm, cols_to_add_cell_norm, KEY_COL, KEY_COL)
-
 
 """
 Export excel files
@@ -476,9 +411,6 @@ write_table_to_excel(writer, df_msdial_summary, 'Summary')
 # # Write simpler summary table with most relevant columns
 df_msdial_summary_output = df_msdial_summary[COLS_TO_KEEP_SUMMARY_OUTPUT]
 write_table_to_excel(writer, df_msdial_summary_output, 'Summary Simple')
-
-# Write the cell pellet normalized stats
-write_table_to_excel(writer, df_msdial_area_cell_norm, 'Cell Norm Stats')
 
 # Write the TIC normalized stats
 write_table_to_excel(writer, df_msdial_norm_tic_stats, 'TIC Norm Stats')

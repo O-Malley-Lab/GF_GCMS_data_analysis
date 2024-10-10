f02cc9d2ea31841658f806c6792739bb6e8e43d4
Lazarina Butkovich <lbutkovich@ucsb.edu>
2024-10-10 14:16:09 -0700

    Script_3_NIST_stats update features to export PNNL matches and NIST23 matches separately. For PNNL dataframe, cleanup and consolidate rows for only 1 signal per unique compound

--- Script_3_NIST_stats.py
+++ Script_3_NIST_stats.py
@@ -4,6 +4,10 @@ Lazarina Butkovich 10/9/24
 
 This script takes in AMDIS NIST processed GCMS data. Additionally, the script analyzes compound matches to (1) the PNNL compound library, with standards run at PNNL on same instrumentation, and (2) NIST search to NIST23 library.
 
+For any given NIST report input for a sample, this script consolidates rows for the same PNNL compound sample name. NIST23 library matches are not included in subsequent analysis but are exported for manual inspection and comparison to potential features of interest.
+
+Then, the script creates and overall dataframe, combining sample abundance data for each compound from each NIST report. Samples are combined based on 'Name' match. A column is added to list the RT and RIs of sample rows combined. The overall dataframe has an 'Area_...' column for each sample (ie: 'Area_AR_1'). The script exports the overall dataframe as an excel file.
+
 """
 
 import pandas as pd
@@ -19,6 +23,122 @@ pd.options.mode.copy_on_write = True
 """""""""""""""""""""""""""""""""""""""""""""
 Functions
 """""""""""""""""""""""""""""""""""""""""""""
+def import_nist_reports_to_dict(report_names_list):
+    """
+    Import NIST reports as pandas dataframes.
+
+    Input: 
+    report_names_list: list of report names (strings) (no .txt)
+
+    Output: 
+    dictionary with report names as keys and pandas dataframes as values
+    """
+    nist_report_dict = {}
+    for nist_report_name in report_names_list:
+        nist_report_path = pjoin(INPUT_FOLDER, nist_report_name + '.txt')
+        nist_report_dict[nist_report_name] = pd.read_csv(nist_report_path, sep='\t', header=0)
+    return nist_report_dict
+
+def generate_nist23_dfs_to_dict(report_names_list, nist_report_dict_raw):
+    """
+    Filter NIST reports to separate NIST23 library matches from PNNL library matches.
+
+    Inputs: 
+    report_names_list: list of report names (strings) (no .txt)
+    nist_report_dict_raw: dictionary with report names as keys and pandas dataframes as values
+
+    Output: dictionary with report names as keys and pandas dataframes as values
+    """
+    nist_report_NIST23_dict = {}
+    for nist_report_name in report_names_list:
+    # Set aside rows with NIST search results
+        nist_report_NIST23 = nist_report_dict_raw[nist_report_name].copy()
+        nist_report_NIST23 = nist_report_NIST23[nist_report_NIST23['Name'].str.startswith('>')]
+        nist_report_NIST23_dict[nist_report_name] = nist_report_NIST23
+    return nist_report_NIST23_dict
+
+def consolidate_rows_in_report(nist_report_PNNL, area_col_name):
+    """
+    For each unique 'Name', keep only the row with the highest 'Net' value. If that is equal, keep the row with the highest area_col_name value. If those match, keep the first row.
+
+    Inputs:
+    nist_report_PNNL: pandas dataframe of a PNNL report
+    area_col_name: name of the area column for the report
+    
+    Outputs: cleaned up pandas dataframe    
+    """
+    # Sort the dataframe by 'Net' and area_col_name in descending order
+    nist_report_PNNL = nist_report_PNNL.sort_values(by=['Net', area_col_name], ascending=[False, False])
+
+    # Drop duplicates based on 'Name' and keep the first row
+    nist_report_PNNL = nist_report_PNNL.drop_duplicates(subset='Name', keep='first')
+    
+    return nist_report_PNNL
+
+def cleanup_PNNL_report(nist_report_PNNL, nist_report_name, vals_to_remove, cols_to_keep):
+    """
+    Pass in a dataframe of a PNNL report and clean up based on specific criteria. To add more filters, adjust this function.
+
+    Inputs:
+    nist_report_PNNL: pandas dataframe of a PNNL report
+    vals_to_remove: list of strings to remove from 'Name' values
+    nist_report_name: name of the report
+
+    Output: cleaned up pandas dataframe
+    """
+    # vals_to_remove are variations of the '?' in 'Name' that indicate confidence levels. However, we do not need this info in the 'Name' column.
+    nist_report_PNNL = nist_report_PNNL.sort_values(by=['RT', 'Net'], ascending=[True, False]).drop_duplicates(subset='RT')
+    for val in vals_to_remove:
+        nist_report_PNNL['Name'] = nist_report_PNNL['Name'].str.replace(val, '')
+    
+    # Use regular expressions to remove patterns '[...] ' or ' [...]'
+    nist_report_PNNL['Name'] = nist_report_PNNL['Name'].str.replace(r'\[.*?\] ', '', regex=True)
+    nist_report_PNNL['Name'] = nist_report_PNNL['Name'].str.replace(r' \[.*?\]', '', regex=True)
+
+    # Remove any trailing ' #' from the 'Name' values
+    nist_report_PNNL['Name'] = nist_report_PNNL['Name'].str.replace(r' \d+$', '', regex=True)
+
+    # keep only the following columns: Name, RT, RI, RI-RI(lib), Net, Weighted, Reverse, (m/z), Area
+    nist_report_PNNL = nist_report_PNNL[cols_to_keep]
+
+    # For each report, rename the 'Area' file to be in the form 'Area_AR_1', for example, where AR_1 comes from the report name (value before the 2nd '_' in the name).
+    report_name = nist_report_name.split('_')[0] + '_' + nist_report_name.split('_')[1]
+    area_col_name = 'Area_' + report_name
+    nist_report_PNNL.rename(columns={'Area': area_col_name}, inplace=True)
+
+    # For each report, make all letters lower case in the 'Name' column
+    nist_report_PNNL['Name'] = nist_report_PNNL['Name'].str.lower()
+
+    # Consolidate rows in the report based on 'Name'. Keep only the row with the highest 'Net' value. If that is equal, keep the row with the highest area_col_name value. If those match, keep the first row.
+    nist_report_PNNL = consolidate_rows_in_report(nist_report_PNNL, area_col_name)
+
+    return nist_report_PNNL
+
+def generate_PNNL_dfs_to_dict(nist_report_dict_raw, report_names_list, vals_to_remove, cols_to_keep):
+    """
+    Clean up each NIST report separately for PNNL in-house library matches. This function uses cleanup_PNNL_report.
+    Inputs: 
+    nist_report_dict_raw: dictionary with report names as keys and pandas dataframes as values
+    report_names_list: list of report names (strings) (no .txt)
+    vals_to_remove: list of strings to remove from 'Name' values
+    cols_to_keep: list of columns to keep in the cleaned up report
+
+    Output: dictionary with report names as keys and pandas dataframes as values   
+    """
+    nist_report_NIST23_dict = {}
+    for nist_report_name in report_names_list:
+
+        # nist_report_cleaned contains only PNNL library matches
+        nist_report_PNNL = nist_report_dict_raw[nist_report_name].copy()
+        nist_report_PNNL = nist_report_PNNL[~nist_report_PNNL['Name'].str.startswith('>')]
+
+        # For values in the 'Name' column, replace any of the strings from vals_to_remove with ''
+        nist_report_PNNL = cleanup_PNNL_report(nist_report_PNNL, nist_report_name, vals_to_remove, cols_to_keep)
+        
+        # Add the cleaned up PNNL report to the dictionary
+        nist_report_NIST23_dict[nist_report_name] = nist_report_PNNL
+
+    return nist_report_NIST23_dict
 
 """""""""""""""""""""""""""""""""""""""""""""
 Values
@@ -33,6 +153,15 @@ NIST_REPORT_GROUP_NAMES = ['AR_1_NIST', 'AR_2_NIST', 'AR_3_NIST', 'AR_4_NIST', '
 # For comparing RI values between features, use a +/- 5 RI cutoff
 RI_CUTOFF = 5
 
+# For cleaning up the PNNL report 'Name' column, remove the following values
+VALS_TO_REMOVE_FROM_NAME_COL = ['? ', '?? ', '??? ', '?']
+
+# For each PNNL report, keep only the following columns: 'Name', 'RT', 'RI', 'RI-RI(lib)', 'Net', 'Weighted', 'Reverse', '(m/z)', 'Area'
+COLS_TO_KEEP_PNNL_REPORT = ['Name', 'RT', 'RI', 'RI-RI(lib)', 'Net', 'Weighted', 'Reverse', '(m/z)', 'Area']
+
+COLS_TO_KEEP_OVERALL_DF = ['Name', 'RT_list', 'RI_list', 'RI-RI(lib)_list', 'Net_list', 'Weighted_list', 'Reverse_list', '(m/z)_list', 'Area_AR_1', 'Area_AR_2', 'Area_AR_3', 'Area_AR_4', 'Area_CC_1', 'Area_CC_2', 'Area_CC_3', 'Area_CC_4', 'Area_MC_1', 'Area_MC_2', 'Area_MC_3', 'Area_MC_4', 'Area_FAMES_1']
+
+
 """""""""""""""""""""""""""""""""""""""""""""
 Main
 """""""""""""""""""""""""""""""""""""""""""""
@@ -40,97 +169,53 @@ Main
 Import NIST reports as pandas dataframes
 """
 # For each NIST report, add the tab-delimited text file data as a pandas dataframe to a dictionary, with the key as the report name
-nist_report_dict = {}
-for nist_report in NIST_REPORT_GROUP_NAMES:
-    nist_report_path = pjoin(INPUT_FOLDER, nist_report + '.txt')
-    nist_report_dict[nist_report] = pd.read_csv(nist_report_path, sep='\t', header=0)
+nist_report_dict_raw = import_nist_reports_to_dict(NIST_REPORT_GROUP_NAMES)
 
 
 """
 Clean up each NIST report separately for PNNL in-house library matches
 """
-# Header:
-# 'FileName', 'CAS', 'Name', 'RT', 'RI', 'Width', 'Purity', 'Model', 'Min. Abund.', 'Amount', 'Scan', 'Peak Tailing', 'S/N (total)', 'Base Peak', 'Max. Amount', 'Area', 'Intgr.Signal', 'Max. Area', 'Extra Width', 'Models', 'Frac. Good', 'Expec. RT', 'RI-RI(lib)', 'Net', 'Weighted', 'Simple', 'Reverse', 'Corrections', '(m/z)', 'S/N (m/z)', 'Area % (m/z)', 'Conc.', 'RT-RT(lib)'
-
-# For each report, we want to consolidate rows within a report, such that:
-# 1) For PNNL library matches, we will remove all rows from the NIST search (rows with a value in the 'Name' column that starts with '>')
-# 2) For all rows with any given 'RT' value, we will only keep the row with the highest Net value 
-# 3) Next, for each row, we will look at the value in the 'Name' column and clean it up if needed. If any of the following are present, remove: '? ', '?? ', '??? ', '[#] ' and ' [#]' where # is any set of characters, usually numbers
-# 4) Some 'Name' values end with a number, in the form of ' #'. This number is not necessary so we will remove the ' #' if it appears as the end of the 'Name' value
-
-# For each report, we will create a new dataframe with the cleaned up data
-nist_report_cleaned_dict = {}
-vals_to_remove = ['? ', '?? ', '??? ', '?']
-for nist_report in NIST_REPORT_GROUP_NAMES:
-    nist_report_cleaned = nist_report_dict[nist_report].copy()
-    nist_report_cleaned = nist_report_cleaned[~nist_report_cleaned['Name'].str.startswith('>')]
-    nist_report_cleaned = nist_report_cleaned.sort_values(by=['RT', 'Net'], ascending=[True, False]).drop_duplicates(subset='RT')
-    
-    # For values in the 'Name' column, replace any of the strings from vals_to_remove with ''
-    for val in vals_to_remove:
-        nist_report_cleaned['Name'] = nist_report_cleaned['Name'].str.replace(val, '')
-    
-    # Use regular expressions to remove patterns '[...] ' or ' [...]'
-    nist_report_cleaned['Name'] = nist_report_cleaned['Name'].str.replace(r'\[.*?\] ', '', regex=True)
-    nist_report_cleaned['Name'] = nist_report_cleaned['Name'].str.replace(r' \[.*?\]', '', regex=True)
-
-    # Remove any trailing ' #' from the 'Name' values
-    nist_report_cleaned['Name'] = nist_report_cleaned['Name'].str.replace(r' \d+$', '', regex=True)
-    
-    nist_report_cleaned_dict[nist_report] = nist_report_cleaned
-
-# For each report in nist_report_cleaned_dict, keep only the following columns: Name, RT, RI, RI-RI(lib), Net, Weighted, Reverse, (m/z)
-for nist_report in NIST_REPORT_GROUP_NAMES:
-    nist_report_cleaned_dict[nist_report] = nist_report_cleaned_dict[nist_report][['Name', 'RT', 'RI', 'RI-RI(lib)', 'Net', 'Weighted', 'Reverse', '(m/z)', 'Amount']]
-
-# The values in the 'Amount' column are strings with a '%' at the end. We will remove the '%' and convert the values to floats. The value should still represent the actual percent value (ie: divide by 100)
-for nist_report in NIST_REPORT_GROUP_NAMES:
-    nist_report_cleaned_dict[nist_report].loc[:, 'Amount'] = nist_report_cleaned_dict[nist_report]['Amount'].str.replace('%', '').astype(float) / 100
-
-# For each report, rename the 'Amount' file to be in the form 'Amount_AR_1', for example, where AR_1 comes from the report name (value before the 2nd '_' in the name).
-for nist_report in NIST_REPORT_GROUP_NAMES:
-    report_name = nist_report.split('_')[0] + '_' + nist_report.split('_')[1]
-    nist_report_cleaned_dict[nist_report].rename(columns={'Amount': 'Amount_' + report_name}, inplace=True)
+# For each report, create a new dataframe with the cleaned up data.
+# Create a separate storage dictionaries for NIST23 library matches and PNNL library matches
+nist_report_NIST23_dict = generate_nist23_dfs_to_dict(NIST_REPORT_GROUP_NAMES, nist_report_dict_raw)
+nist_report_PNNL_dict = generate_PNNL_dfs_to_dict(nist_report_dict_raw, NIST_REPORT_GROUP_NAMES, VALS_TO_REMOVE_FROM_NAME_COL, COLS_TO_KEEP_PNNL_REPORT)
 
 
-# """
-# Export cleaned up NIST reports
-# """
-# # For each cleaned up report, export as an excel file
-# for nist_report in NIST_REPORT_GROUP_NAMES:
-#     nist_report_cleaned_path = pjoin(TEMP_FOLDER, nist_report + '_cleaned.xlsx')
-#     nist_report_cleaned_dict[nist_report].to_excel(nist_report_cleaned_path, index=False)
+"""
+Export NIST reports per sample, for matches to NIST23 library and PNNL in-house library
+"""
+# For each report, export as an excel file
+for nist_report_name in NIST_REPORT_GROUP_NAMES:
+    nist_report_NIST23_dict[nist_report_name].to_excel(pjoin(TEMP_FOLDER, nist_report_name + '23_matches.xlsx'), index=False)
+    nist_report_PNNL_dict[nist_report_name].to_excel(pjoin(TEMP_FOLDER, nist_report_name + '_PNNL_matches.xlsx'), index=False)
+
+
+
+
+
+
+"""
+Consolidate Reports to Overall Dataframe
+"""
+# We want to consolidate the cleaned up NIST reports into one dataframe. We will match features based on 'Name'. 
+# Create overall_df, with the following header: 'Name', 'RT_list', 'RI_list', 'RI-RI(lib)_list', 'Net_list', 'Weighted_list', 'Reverse_list', '(m/z)_list', 'Area_AR_1', 'Area_AR_2', 'Area_AR_3', 'Area_AR_4', 'Area_CC_1', 'Area_CC_2', 'Area_CC_3', 'Area_CC_4', 'Area_MC_1', 'Area_MC_2', 'Area_MC_3', 'Area_MC_4', 'Area_FAMES_1'
+
+overall_df = pd.DataFrame(columns=COLS_TO_KEEP_OVERALL_DF)
+
+# Generate a list of all unique 'Name' values from all reports.
+all_PNNL_compound_names_detected = []
+
+for nist_report_name in NIST_REPORT_GROUP_NAMES:
+    all_PNNL_compound_names_detected.extend(nist_report_PNNL_dict[nist_report_name]['Name'].tolist())
+
+all_PNNL_compound_names_detected = list(set(all_PNNL_compound_names_detected))
+
+# For each unique 'Name', create a row in the overall_df
 
 """
 Perform Fatty Acid Profiling for Each Sample
 """
 
-
-
-
-# """
-# Consolidate Reports to Overall Dataframe
-# """
-# Next, we want to consolidate the cleaned up NIST reports into one dataframe. We will match features based on values in 'RI' (+/- RI_CUTOFF) and 'Name'. The overall dataframe (overall_df) will start as a copy of the first cleaned up report. For each subsequent report, we will add a corresponding "Amount_..." column and iterate through the rows in overall_df to find any matches. If there is a match, we will add the value from the 'Amount_...' column to the matching row. If there is not a match, we will add a new row to overall_df with the values from the report and 0 value for previous 'Amount_...' columns. We will iterate over all reports in NIST_REPORT_GROUP_NAMES to create the overall_df.
-
-# # Start with the first cleaned up report
-# overall_df = nist_report_cleaned_dict[NIST_REPORT_GROUP_NAMES[0]].copy()
-
-# # For each subsequent report, add the 'Amount_...' column to the overall_df
-# cols_to_add = ['Amount_' + nist_report.split('_')[0] + '_' + nist_report.split('_')[1] for nist_report in NIST_REPORT_GROUP_NAMES]
-
-# i=0
-# for col in cols_to_add:
-#     if i == 0:
-#         # skip for the first report
-#         i+=1
-#         continue
-#     # Add the 'Amount_...' column to the overall_df. the values will be nan for now
-#     overall_df[col] = np.nan
-
-
-# # For each subsequent report, iterate through the rows in overall_df to find matches
-
-# # Export the overall_df as an excel file
-# overall_df_path = pjoin(TEMP_FOLDER, 'overall_PNNL_lib_matches.xlsx')
-# overall_df.to_excel(overall_df_path, index=False)
+# Export the overall_df as an excel file
+overall_df_path = pjoin(TEMP_FOLDER, 'overall_PNNL_lib_matches.xlsx')
+overall_df.to_excel(overall_df_path, index=False)
--- Script_3_option2_NIST_Fatty_Acid_Profiling.py
+++ Script_3_option2_NIST_Fatty_Acid_Profiling.py
@@ -33,20 +33,49 @@ NIST_REPORT_GROUP_NAMES = ['AR_1_NIST', 'AR_2_NIST', 'AR_3_NIST', 'AR_4_NIST', '
 # For comparing RI values between features, use a +/- 5 RI cutoff
 RI_CUTOFF = 5
 
-# Fatty Acids of Interest
+"""
+Notes for Fatty Acids to Profile
+"""
+# FAMES related
+# 'methyl caprylate' - C8 in FAMES
+# 'methyl caprate' - C10 in FAMES
+# 'methyl laurate' - C12 in FAMES
+# 'methyl myristate' - C14 in FAMES
+# 'methyl palmitate' - C16 in FAMES
+# 'methyl stearate' - C18 in FAMES
+# 'methyl eicosanoate' - C20 in FAMES
+# 'methyl docosanoate' - C22 in FAMES
+# 'methyl linocerate' - C24 in FAMES
+# 'methyl hexacosanoate' - C26 in FAMES
+# 'methyl octacosanoate' - C28 in FAMES
+
+# PNNL library, FAs from Kar et al. GCMS profiling for anaerobic gut fungi
+# 'capric acid' - C10:0, Decanoic
+# 'lauric acid' -  C12:0, Lauric
+# cannot find in PNNL lib - C13:0, Tridecanoic
+# 'myristic acid' - C14:0, Myristic
+# cannot find in PNNL lib - C15:0, Pentadecanoic
+# 'palmitic acid' - C16:0, Palmitic
+# 'palmitoleic acid' - C16:1, Palmitoleic
+# 'stearic acid' - C18:0, Stearic
+# 'oleic acid' - C18:1n9c, Oleic
+# 'linoleic acid' - C18:2n6c, Linoleic
+# 'arachidic acid' - C20:0, Arachidic
+# cannot find in PNNL lib - C20:1, Eicosenoic
+# cannot find in PNNL lib - C20:4n6, Arachidonic
+# cannot find in PNNL lib - C21:0, Heneicosanoic
+# cannot find in PNNL lib - C22:1n9, Erucic
+# 'docosahexaenoic acid' - C22:6n3, Cis-4,7,10,13,16,19- Docosahexaenoic
+# cannot find in PNNL lib - C24:1, Nervonic
+
+# Optional to add: NIST library, FAs from Kar et al. GCMS profiling for anaerobic gut fungi
+# '>Pentadecanoic acid, TMS derivative' - C15:0, Pentadecanoic
+# '>Heneicosanoic acid, TMS derivative' - C21:0, Heneicosanoic
+# ...
+
+# Fatty Acids of Interest, as they appear in PNNL in-house library
 FATTY_ACIDS_LIST = [
-    'Methyl Caprylate',
-    'Methyl Pelargonate',
-    'Methyl Caprate',
-    'Methyl Laurate',
-    'Methyl Myristate',
-    'Methyl Palmitate',
-    'Methyl Stearate',
-    'Methyl Eicosanoate',
-    'Methyl Docosanoate',
-    'Methyl Linocerate',
-    'Methyl Hexacosanoate',
-    'Methyl Octacosanoate',
+    'methyl caprylate' , 'methyl caprate', 'methyl laurate', 'methyl myristate', 'methyl palmitate', 'methyl stearate', 'methyl eicosanoate', 'methyl docosanoate', 'methyl linocerate', 'methyl hexacosanoate', 'methyl octacosanoate', 'capric acid', 'lauric acid', 'myristic acid', 'palmitic acid', 'palmitoleic acid', 'stearic acid', 'oleic acid', 'linoleic acid', 'arachidic acid', 'docosahexaenoic acid'
 ]
 
 """""""""""""""""""""""""""""""""""""""""""""
@@ -73,6 +102,7 @@ Clean up each NIST report separately for PNNL in-house library matches
 # 2) For all rows with any given 'RT' value, we will only keep the row with the highest Net value 
 # 3) Next, for each row, we will look at the value in the 'Name' column and clean it up if needed. If any of the following are present, remove: '? ', '?? ', '??? ', '[#] ' and ' [#]' where # is any set of characters, usually numbers
 # 4) Some 'Name' values end with a number, in the form of ' #'. This number is not necessary so we will remove the ' #' if it appears as the end of the 'Name' value
+# 5) Make all letters in the 'Name' column lower case
 
 # For each report, we will create a new dataframe with the cleaned up data
 nist_report_cleaned_dict = {}
@@ -97,25 +127,24 @@ for nist_report in NIST_REPORT_GROUP_NAMES:
 
 # For each report in nist_report_cleaned_dict, keep only the following columns: Name, RT, RI, RI-RI(lib), Net, Weighted, Reverse, (m/z)
 for nist_report in NIST_REPORT_GROUP_NAMES:
-    nist_report_cleaned_dict[nist_report] = nist_report_cleaned_dict[nist_report][['Name', 'RT', 'RI', 'RI-RI(lib)', 'Net', 'Weighted', 'Reverse', '(m/z)', 'Amount']]
+    nist_report_cleaned_dict[nist_report] = nist_report_cleaned_dict[nist_report][['Name', 'RT', 'RI', 'RI-RI(lib)', 'Net', 'Weighted', 'Reverse', '(m/z)', 'Area']]
 
-# The values in the 'Amount' column are strings with a '%' at the end. We will remove the '%' and convert the values to floats. The value should still represent the actual percent value (ie: divide by 100)
-for nist_report in NIST_REPORT_GROUP_NAMES:
-    nist_report_cleaned_dict[nist_report].loc[:, 'Amount'] = nist_report_cleaned_dict[nist_report]['Amount'].str.replace('%', '').astype(float) / 100
-
-# For each report, rename the 'Amount' file to be in the form 'Amount_AR_1', for example, where AR_1 comes from the report name (value before the 2nd '_' in the name).
+# For each report, rename the 'Area' file to be in the form 'Area_AR_1', for example, where AR_1 comes from the report name (value before the 2nd '_' in the name).
 for nist_report in NIST_REPORT_GROUP_NAMES:
     report_name = nist_report.split('_')[0] + '_' + nist_report.split('_')[1]
-    nist_report_cleaned_dict[nist_report].rename(columns={'Amount': 'Amount_' + report_name}, inplace=True)
+    nist_report_cleaned_dict[nist_report].rename(columns={'Area': 'Area_' + report_name}, inplace=True)
 
+# For each report, make all letters lower case in the 'Name' column
+for nist_report in NIST_REPORT_GROUP_NAMES:
+    nist_report_cleaned_dict[nist_report]['Name'] = nist_report_cleaned_dict[nist_report]['Name'].str.lower()
 
-# """
-# Export cleaned up NIST reports
-# """
-# # For each cleaned up report, export as an excel file
-# for nist_report in NIST_REPORT_GROUP_NAMES:
-#     nist_report_cleaned_path = pjoin(TEMP_FOLDER, nist_report + '_cleaned.xlsx')
-#     nist_report_cleaned_dict[nist_report].to_excel(nist_report_cleaned_path, index=False)
+"""
+Export cleaned up NIST reports
+"""
+# For each cleaned up report, export as an excel file
+for nist_report in NIST_REPORT_GROUP_NAMES:
+    nist_report_cleaned_path = pjoin(TEMP_FOLDER, nist_report + '_cleaned.xlsx')
+    nist_report_cleaned_dict[nist_report].to_excel(nist_report_cleaned_path, index=False)
 
 """
 Perform Fatty Acid Profiling for Each Sample
@@ -123,30 +152,3 @@ Perform Fatty Acid Profiling for Each Sample
 
 
 
-
-# """
-# Consolidate Reports to Overall Dataframe
-# """
-# Next, we want to consolidate the cleaned up NIST reports into one dataframe. We will match features based on values in 'RI' (+/- RI_CUTOFF) and 'Name'. The overall dataframe (overall_df) will start as a copy of the first cleaned up report. For each subsequent report, we will add a corresponding "Amount_..." column and iterate through the rows in overall_df to find any matches. If there is a match, we will add the value from the 'Amount_...' column to the matching row. If there is not a match, we will add a new row to overall_df with the values from the report and 0 value for previous 'Amount_...' columns. We will iterate over all reports in NIST_REPORT_GROUP_NAMES to create the overall_df.
-
-# # Start with the first cleaned up report
-# overall_df = nist_report_cleaned_dict[NIST_REPORT_GROUP_NAMES[0]].copy()
-
-# # For each subsequent report, add the 'Amount_...' column to the overall_df
-# cols_to_add = ['Amount_' + nist_report.split('_')[0] + '_' + nist_report.split('_')[1] for nist_report in NIST_REPORT_GROUP_NAMES]
-
-# i=0
-# for col in cols_to_add:
-#     if i == 0:
-#         # skip for the first report
-#         i+=1
-#         continue
-#     # Add the 'Amount_...' column to the overall_df. the values will be nan for now
-#     overall_df[col] = np.nan
-
-
-# # For each subsequent report, iterate through the rows in overall_df to find matches
-
-# # Export the overall_df as an excel file
-# overall_df_path = pjoin(TEMP_FOLDER, 'overall_PNNL_lib_matches.xlsx')
-# overall_df.to_excel(overall_df_path, index=False)
